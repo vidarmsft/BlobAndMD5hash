10ea3c62e5ad714809d7cb94d11473036966a804
Vivek <vidar@microsoft.com>
2018-08-25 00:25:35 -0700

    Added the ps file for blob and md5

---
+++ md5blob2.ps1
@@ -0,0 +1,16 @@
+﻿#This script enumerates blob names and corresponding MD5 hashes as stored in the given storage account container.
+#The output is stored in a csv file, you can specify the path of the file.
+$storageAccountName = Read-Host "Please enter storage account name:"
+$storageAccountKey = Read-Host "Please enter storage account key:"
+$containerName = Read-Host "Please enter container name where blobs are stored:"
+$path = Read-Host "Please enter path for csv output of the blob name and MD5:"
+$ctx = New-AzureStorageContext -StorageAccountName $storageAccountName -StorageAccountKey $storageAccountKey
+$blobs = Get-AzureStorageBlob -Context $ctx -Container $containerName
+foreach ($blob in $blobs)
+{
+$cloudBlockBlob = [Microsoft.WindowsAzure.Storage.Blob.CloudBlockBlob]$blob.ICloudBlob
+$outputline = $cloudBlockBlob.Name + "," + $cloudBlockBlob.Properties.ContentMD5
+Add-Content -Path $path -Value $outputline
+}
+
+
